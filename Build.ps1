810f783755599c8203471ebf1a101c5a274651bf
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-12 21:58:55 +0100

    Created a example build script showing using GitVersion

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,13 @@
 $currentDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $output = . "$currentDir\GitVersion.1.0.0.0\tools\GitVersion.exe"
+
 $joined = $output -join "`n"
 $versionInfo = $joined | ConvertFrom-Json
+$version = $versionInfo.SemVer
 
 (Get-Content "$currentDir\src\UsefulStuff.psm1") | 
-    Foreach-Object {$_ -replace '__version__',"v$versionInfo.SemVer"} |
+    Foreach-Object {$_ -replace '__version__',"v$version"} |
     Out-File "$currentDir\src\UsefulStuff.psm1"
+    
+Write-Output "##teamcity[buildNumber '#$version']"
